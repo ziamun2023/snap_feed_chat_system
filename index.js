21ed2076869403d9d6cd2a7ad8e9f1766988632e
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-06-19 21:15:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const { default: mongoose } = require('mongoose')
+var mongooes=require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/snapfeed-chat')
+
+const app=require('express')()
+const http=require('http').Server(app)
+http.listen(3000,function(){
+    console.log('serrver is running')
+})
